2612c356f58368b8df234bc7b109d6f622082ebc
Python <Python@Admins-iMac-5.local>
2019-07-02 18:38:11 +0700

    Triangle03

--- Triangle2.py
+++ Triangle2.py
@@ -1,7 +1,7 @@
 import math
 a = float(input('ความยาวด้าน a : '))
 b = float(input('ความยาวด้าน b : '))
-D = float(input())
+D = float(input('มุม C : '))
 C = D*math.pi/180
 c = math.sqrt(a**2+b**2-2*a*b*math.cos(C))
 print('ความยาวด้าน c : ',c)

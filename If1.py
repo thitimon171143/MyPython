09a5814009b18ad51549bb72f083047723eca1f0
Python <Python@Admins-iMac-5.local>
2019-07-09 15:26:17 +0700

    FirstIf1

---
+++ If1.py
@@ -0,0 +1,7 @@
+score1 = int(input('First Test Score : '))
+score2 = int(input('Second Test Score : '))
+score3 = int(input('Third Test Score : '))
+average = (score1+score2+score3)/3
+print('Average = ',average)
+if average > 95:
+    print('Congratulate')

8b804b6633fc4c1d082239833e4a740606065f03
unknown <montha.kam.iam@gmail.com>
2019-07-10 17:26:59 +0700

    finally

--- parkcar.py
+++ parkcar.py
@@ -1,13 +1,36 @@
+import math
 hrintime = int(input('Hour in : '))
 minintime = int(input('Minute in : '))
 hrouttime = int(input('Hour out : '))
 minouttime = int(input('Minute out : '))
-if hrintime==0 and minintime <=15 :
-    price = ('0')
-elif hrintime<=3 and minintime>=16 :
-    price = ('10')
-elif hrintime>=4 and minintime>=16 :
-    price = ('20')
-elif hrintime>=6 and minintime>=16 :
-    price = ('200')
-print('Total : ',price)
+
+in_timemin = (hrintime*60)+minintime
+out_timemin = (hrouttime*60)+minouttime
+
+#cal
+time_hr = hrouttime-hrintime
+time_min = minouttime-minintime
+summin= (time_hr*60)+time_min
+
+#open
+x = 0
+if in_timemin<7*60 or in_timemin>23*60 :
+    print("Can't in Cosed")
+    x = 1
+if out_timemin<7*60 or out_timemin>23*60 :
+    print("Can't out Closed")
+    x = 1
+
+if x==0 :
+    if summin <= 15:
+        print('0')
+    elif summin<=3*60:
+        print(10*int(math.ceil(summin/60)))
+    elif summin<=6*60:
+        print(20*int(math.ceil(summin/60)))
+    elif summin>6*60:
+        print('200')
+    else:
+        'Error'
+else:
+    print('Pls new')

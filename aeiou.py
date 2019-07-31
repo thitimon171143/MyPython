5e2f158f188fe573df3d3c948f353dab826ec5d2
thitimon171143 <montha.kam.iam@gmail.com>
2019-07-31 11:48:21 +0700

    homework-aeiou

--- aeiou.py
+++ aeiou.py
@@ -1,8 +1,35 @@
+'''
 sentence = input('Enter word : ')
 count = 0
 
 for x in sentence :
-    if x in 'aeiou' :
+        if x in 'a' or 'e' or 'i'
+         if x in 'aeiou' :
         count += 1
 
 print(count)
+'''
+
+'''
+in1 = input(":")
+Te = ['a','u','i','o','e']
+n = 0
+i = 0
+up= 0
+num = 0
+
+len_in = len(in1)
+len_Te = len(Te)
+
+for i in range(len_in) :
+    for ii in range(len_Te) :
+        if in1[i] in Te[ii]:
+            n = 1
+    if n == 1 and up == 0:
+        num += 1
+        up = 1
+    elif n == 0 :
+        up = 0
+    n = 0
+print (num)
+'''
